29cbfb8aada1630564bac3e0234d0f348e10f9d8
devWizBiz <arrowe686@gmail.com>
2024-10-04 15:42:53 -0500

    Updated Refactored Parser py file

--- parserRefactor.py
+++ parserRefactor.py
@@ -5,50 +5,77 @@ import re
 # Internal Libraries
 import support
 
+# Global
+SymbolTable = {"Global": {}}
+AST = {"Program": {}}
+
 PARSER_PATTERNS = [
-    ('FUNCTION_DECLARATION', r'(void||int||float||char)IDENTIFIER)\(\)\{\}'),
-    ('VARIABLE_DECLARATION', r'(int||float||char)IDENTIFIER;'),
-    ('VARIABLE_ASSIGNMENT_FLOAT', r'floatIDENTIFIER=FLOATING_CONSTANT;'),
-    ('VARIABLE_ASSIGNMENT_INT', r'intIDENTIFIER=INTEGER_CONSTANT;'),
-    ('VARIABLE_ASSIGNMENT_CHAR', r'charIDENTIFIER=CHAR_CONSTANT;'),
-    ('VARIABLE_ASSIGNMENT_STRING', r'charIDENTIFIER\[\]=STRING_CONSTANT;'),
+    ('GLOBAL_FUNCTION_DECLARATION', r'(void||int||float||char)IDENTIFIER\(\)\{\}'),
+    ('GLOBAL_FUNCTION_DECLARATION_PARTIAL', r'(void||int||float||char)IDENTIFIER\(\)\{'),
+    ('GLOBAL_VARIABLE_DECLARATION', r'(int||float||char)IDENTIFIER;'),
+    ('GLOBAL_VARIABLE_ASSIGNMENT_FLOAT', r'floatIDENTIFIER=FLOATING_CONSTANT;'),
+    ('GLOBAL_VARIABLE_ASSIGNMENT_INT', r'intIDENTIFIER=INTEGER_CONSTANT;'),
+    ('GLOBAL_VARIABLE_ASSIGNMENT_CHAR', r'charIDENTIFIER=CHAR_CONSTANT;'),
+    ('GLOBAL_VARIABLE_ASSIGNMENT_STRING', r'charIDENTIFIER\[\]=STRING_CONSTANT;'),
 ]
 
-# Join the token patterns through the OR operator along with it's sub-pattern name
+# Join the tokenPos patterns through the OR operator along with it's sub-pattern name
 PATTERNS_COMBINED = '|'.join('(?P<%s>%s)' % pair for pair in PARSER_PATTERNS)
 COMPILED_PATTERNS = re.compile(PATTERNS_COMBINED)
 
 def parserProgram(tokens):
-    tokensLeft = len(tokens)
-    parseFunctions(tokens, tokensLeft)
+    tokenPos = 0
+    parseFunctions(tokens, tokenPos)
+    return AST, SymbolTable
 
-def parseFunctions(tokens, tokensLeft):
+def parseFunctions(tokens, tokenPos):
 
     # Check against RegEx    
     searchString = ""
-    for token in tokens:
+    tokensInfo = []
+    while tokenPos < len(tokens):
 
-        if tokens[token]['TYPE'] in ['KEYWORD', 'PUNCTUATORS']:
-            searchValue = tokens[token]['TOKEN']
+        if tokens[tokenPos]['TYPE'] in ['KEYWORD', 'PUNCTUATORS']:
+            searchValue = tokens[tokenPos]['TOKEN']
         else:
-            searchValue = tokens[token]['TYPE']
-        searchString += searchValue
+            searchValue = tokens[tokenPos]['TYPE']
         
-        for match in COMPILED_PATTERNS.finditer(searchString):
-            print(f'Test String: {searchString}')
-            print(f'Matched string: {match.group()}')
-            print(f'Matched Type: {match.lastgroup}')
-            print(f'Start position: {match.start()}')
-            print(f'End position: {match.end()}')
-            print('---')
-            searchString = "" # Reset
-        else:
-            pdb.set_trace()
+        searchString += searchValue
+        tokensInfo.append((tokens[tokenPos]['TOKEN'], tokens[tokenPos]['TYPE']))
+        
+        match = COMPILED_PATTERNS.search(searchString)
+        if not match:
+            tokenPos += 1
+            continue
     
-    parseStatements()
+        if match.lastgroup == 'GLOBAL_VARIABLE_DECLARATION':
+            SymbolTable["Global"][tokensInfo[1][0]] = tokensInfo[0][0]
+        
+        elif match.lastgroup in ['GLOBAL_VARIABLE_ASSIGNMENT_FLOAT', 'GLOBAL_VARIABLE_ASSIGNMENT_INT', 
+                                'GLOBAL_VARIABLE_ASSIGNMENT_CHAR', 'GLOBAL_VARIABLE_ASSIGNMENT_STRING']:
+            SymbolTable["Global"][tokensInfo[1][0]] = tokensInfo[0][0]
+        
+        elif match.lastgroup == 'GLOBAL_FUNCTION_DECLARATION_PARTIAL':
+            lookingIndex = lookAhead(tokens, tokenPos, '}')
+            
+            if lookingIndex != -1:
+                searchString += tokens[lookingIndex]['TOKEN']
+                match = COMPILED_PATTERNS.search(searchString)
+            
+                if match.lastgroup == 'GLOBAL_FUNCTION_DECLARATION':
+                    SymbolTable["Global"][tokensInfo[1][0]] = {'retType':tokensInfo[0][0], "args":"None", "vars":{}}
+                    tokenPos = parseStatements(tokens, tokenPos)
+                    parseFunctions(tokens, tokenPos)
+                    break               
+        
+        tokenPos += 1
+        searchString = "" # Reset the search string
+        tokensInfo = [] # Reset the tokens info
 
-def parseStatements():
-    pass
+
+def parseStatements( tokens, tokenPos):
+    lookAheadIndex = lookAhead(tokens, tokenPos, '}') # STUBBED
+    return lookAheadIndex + 1
 
 def checkExpression():
     pass
@@ -56,6 +83,6 @@ def checkExpression():
 def lookAhead(tokens, tokenIndex, searchToken):
     while tokenIndex < len(tokens):
         if tokens[tokenIndex]['TOKEN'] == searchToken:
-            return tokenIndex  # Return index of the search Token
+            return tokenIndex  # Return index of the search tokenPos
         tokenIndex += 1
     return -1  # Return -1 
